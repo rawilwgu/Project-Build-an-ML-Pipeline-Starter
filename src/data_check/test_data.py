084db58bee2730459d3389a6204401c2e4674abe
Rachel <rawilwgu@Galadriel>
2024-07-01 21:15:40 -0500

    Step 3: Data Testing

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+
+def test_row_count(data: pd.DataFrame):
+    """
+    Test if dataset has correct number of rows
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+    Test if price is in the correct range
+    """
+    assert data['price'].between(min_price, max_price).all()
